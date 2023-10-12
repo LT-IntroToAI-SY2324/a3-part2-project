515c4302283e52aeb41bd9262d313f4e9c4a2fd0
CHerrera63 <cherrera63@cps.edu>
2023-10-12 10:04:45 -0500

    created a list

---
+++ list.py
@@ -0,0 +1,94 @@
+from typing import List, Tuple
+
+mylist_db: List[Tuple[str, str, int, List[str]]] = [
+    (
+        "lane tech",  # school
+        "roscoe",  # neighborhood
+        5,  # amount of places?????????? might get rid of
+        [
+            "mcdonalds",
+            "popeyes",
+            "chop suey",
+            "wendys",
+            "dunkin donuts",
+        ],  # places to eat
+    ),
+    (
+        "whitney young",
+        "west side",
+        3,
+        [
+            "cafe l'appetito",
+            "froth",
+            "la lagartija taqueria",
+        ],
+    ),
+    (
+        "jones",
+        "printer's row",
+        4,
+        [
+            "devil dawgs on state",
+            "harold's chicken shack",
+            "starbucks",
+            "gong cha",
+        ],
+    ),
+    (
+        "cristo rey",
+        "west side",
+        5,
+        [
+            "xurro",
+            "carniceria maribel",
+            "mole village",
+            "subway",
+            "tj's",
+        ],
+    ),
+    (
+        "de la salle",
+        "bronzeville",
+        5,
+        ["harold's chicken", "starbucks", "mcdonalds", "kfc", "sharks fish and chicken",],
+    ),
+    (
+        "westinghouse",
+        "garfield park",
+        3,
+        ["popeyes", "best sub", "whale's fish and chicken",],
+    ),
+    (
+        "taft",
+        "norwood park",
+        4,
+        [
+            "saint bess jerk",
+            "sapori napoletani pizzeria napoletana",
+            "cozy corner restaurant and pancake house",
+            "norwood restaurant",
+        ],
+    ),
+    (
+        "lakeview",
+        "lake view",
+        4,
+        [
+            "popeyes",
+            "starbucks",
+            "the piggery",
+            "rockin taco",
+        ],
+    ),
+    (
+        "von steuben",
+        "north park",
+        4,
+        [
+            "taqueria las flores",
+            "starbucks",
+            "ban po jung",
+            "popeyes",
+        ],
+    )
+]
